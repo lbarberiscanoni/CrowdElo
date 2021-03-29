eabe7ddc4b5fb8c0890a0bf8cae97ac3fe93a4c4
lbarberiscanoni <lorenzo.barberis.canonico@gmail.com>
2021-03-29 15:54:06 -0400

    implemented the elo-rating system that updates the correct dimension on firebase

--- pages/sources/startups.js
+++ pages/sources/startups.js
@@ -28,6 +28,12 @@ const Startups = () => {
 	const [snapshots, loading, error] = useList(firebase.database().ref('/alice/items'));
 	const [companies, changeCompanies] = useState([])
 
+	let dimension = {}
+
+	useEffect(() => {
+		dimension = window.location.href.split("?")[1]
+	})
+
 	const findPair = () => {
 		let numOfStartups = snapshots.length
 
@@ -38,7 +44,47 @@ const Startups = () => {
 			secondCo = Math.floor(Math.random() * (numOfStartups - 0 + 1) ) + 0
 		}
 
-		return [snapshots[firstCo].val(), snapshots[secondCo].val()]
+		return [{"co": snapshots[firstCo].val(), "key": firstCo}, {"co": snapshots[secondCo].val(), "key": secondCo }]
+	}
+
+	const elo = (playerScore, opponentScore, winStatus) => {
+		//https://en.wikipedia.org/wiki/Elo_rating_system#Mathematical_details
+
+		const k_factor = 15
+
+		const spread = opponentScore - playerScore
+		const normalized_spread = spread / 400
+		const expectation = 1 / (1 + (10 ** normalized_spread))
+		const adjusted_score = playerScore + (k_factor * (winStatus - expectation))
+
+		return adjusted_score
+	}
+
+	const updateMatchup = (winnerRef) => {
+		const loserRef = 1 - winnerRef
+		const winner_key = companies[winnerRef]["key"]
+		const loser_key = companies[loserRef]["key"]
+		const currentScore_winner = snapshots[winner_key].val()["dimensions"][dimension]
+		const currentScore_loser = snapshots[loser_key].val()["dimensions"][dimension]
+		const updatedScore_winner = elo(currentScore_winner, currentScore_loser, 1)
+		const updatedScore_loser = elo(currentScore_loser, currentScore_winner, 0)
+
+		let update = {}
+		update[dimension] = updatedScore_winner
+		firebase.database()
+			.ref('/alice/items')
+			.child(winner_key)
+			.child("dimensions")
+			.update(update)
+
+		update[dimension] = updatedScore_loser
+		firebase.database()
+			.ref('/alice/items')
+			.child(loser_key)
+			.child("dimensions")
+			.update(update)
+
+		changeCompanies(findPair())
 	}
 
 	if (snapshots.length > 0) {
@@ -54,19 +100,19 @@ const Startups = () => {
 				{ companies.length > 0 ? <Row>
 						<Col xs="auto">
 							<a 
-								onClick={() => changeCompanies(findPair())}
+								onClick={() => updateMatchup(0)}
 							>
 								<Item 
-									info={companies[0].info} 
+									info={companies[0]["co"].info} 
 								/>
 							</a>
 						</Col>
 						<Col xs="auto">
 							<a
-								onClick={() => changeCompanies(findPair())}
+								onClick={() => updateMatchup(1)}
 							>
 								<Item 
-									info={companies[1].info} 
+									info={companies[1]["co"].info} 
 								/>
 							</a>
 						</Col>
